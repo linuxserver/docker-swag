8d566e1c99d0ab3b6f882c72e14b59b6f759b143
Agung RB <agungrbudiman@gmail.com>
2021-05-22 17:19:51 +0700

    Add dns alias support

--- root/app/le-renew.sh
+++ root/app/le-renew.sh
@@ -7,7 +7,7 @@ echo
 echo "<------------------------------------------------->"
 echo "cronjob running on "$(date)
 echo "Running certbot renew"
-if [ "$ORIGVALIDATION" = "dns" ] || [ "$ORIGVALIDATION" = "duckdns" ]; then
+if [ "$ORIGVALIDATION" = "dns" ] || [ "$ORIGVALIDATION" = "duckdns" ] || [ "$ORIGVALIDATION" = "dns-alias" ]; then
   certbot -n renew \
     --post-hook "if ps aux | grep [n]ginx: > /dev/null; then s6-svc -h /var/run/s6/services/nginx; fi; \
     cd /config/keys/letsencrypt && \
